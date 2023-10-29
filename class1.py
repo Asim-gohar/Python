bd36973a0980f197a27fc12fae5a935bc6522e36
Asim-gohar <asimbutt9664@gmail.com>
2023-10-29 23:37:36 +0500

    modren python

---
+++ class1.py
@@ -0,0 +1,10 @@
+# name : int = 286 
+# print(name)
+# print(type(name))
+# name :str = "39874" 
+# safe : int = 21131
+# print(name)
+from IPython.display import display
+a = 7
+b = 6  
+display("Pakistan value a = {} and Zindabad value b = {}".format(b,a))
